d4bd8541a0f17228404434fc3fe44dd7588a04f5
vLamass <viniciusslamas17@gmail.com>
2026-04-08 11:35:48 -0300

    08/04/26 - Criação do Enum Parentesco

--- src/sistema/enums/Parentesco.java
+++ src/sistema/enums/Parentesco.java
@@ -1,5 +1,16 @@
 package sistema.enums;
 
-public class Parentesco {
-  
+public enum Parentesco {
+    
+    FILHO(1), SOBRINHO(2), OUTROS(3);
+
+    private final int valor;
+    Parentesco(int valorOpcao){
+        this.valor = valorOpcao;
+    }
+    public int getValor(){
+        return this.valor;
+    }
+
 }
+        
