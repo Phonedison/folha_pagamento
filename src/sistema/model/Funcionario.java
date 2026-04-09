12a1237bd7ba95adfd00e037bd216bfe380c501d
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-09 14:21:47 -0300

    Adicionando atributos, gets e sets

--- src/sistema/model/Funcionario.java
+++ src/sistema/model/Funcionario.java
@@ -1,6 +1,34 @@
 package sistema.model;
 
 public class Funcionario extends Pessoa {
-  
-  
+  private double salarioBruto;
+  private double descontoInss;
+  private double descontoIR;
+
+  //   Get
+    public double getDescontoInss() {
+        return descontoInss;
+    }
+
+    public double getDescontoIR() {
+        return descontoIR;
+    }
+
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+//    Set
+
+    public void setDescontoInss(double descontoInss) {
+        this.descontoInss = descontoInss;
+    }
+
+    public void setDescontoIR(double descontoIR) {
+        this.descontoIR = descontoIR;
+    }
+
+    public void setSalarioBruto(double salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
 }
+
