ee2950c5e93bf7f20bb94ec578aced951f8d6054
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-12 16:59:03 -0300

    Intanciação do método de menu na classe main e execução do método de verificação e criação de tabelas.

--- src/sistema/app/Main.java
+++ src/sistema/app/Main.java
@@ -1,4 +1,10 @@
 package sistema.app;
+import scripts_backup.Menus;
+import sistema.repository.InicializarDB;
+
+import sistema.repository.ConexaoDB;
+
+
 
 public class Main {
     /**
@@ -6,19 +12,19 @@ public class Main {
      * @throws Exception
      */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         /* Conexao DB */
 
-        // Método para gerar a conexao do Banco
-        // ConexaoDB conexao = new ConexaoDB (
-        // (int porta), // porta que configurou
-        // (String meuDb), // nome do DB que deu
-        // (String usuario), // usuario que definiu para mexer no DB
-        // (String senha)) // a senha de acesso ao DB
+    // Método para gerar a conexao do Banco
 
-        // comando para executar verificacao e criação das tabelas
-        // InicializarDB.inicializar(conexao);
+        Menus conexao = new Menus();
+        conexao.menuConexao();
+
+    // Comando para executar verificacao e criação das tabelas
+
+        InicializarDB.inicializar(conexao.menuConexao());
+
+
 
         // ------------------------------------------------------------------------//
 
