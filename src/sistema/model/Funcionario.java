2797434659055c245bdbf6f8254946e70f8c58fa
joaopedrobr3 <joaopedrobr3@gmail.com>
2026-04-13 11:33:51 -0300

    Removendo dependentes do parâmentro do construtor.

--- src/sistema/model/Funcionario.java
+++ src/sistema/model/Funcionario.java
@@ -5,46 +5,45 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Funcionario extends Pessoa {
-  
+
   private int id_funcionario;
   private double salarioBruto;
   // private double descontoInss;
   // private double descontoIR;
-  private Set<Dependente> dependentes = new HashSet<>();
-  /* 
-  
-  *  Set <tipo da lista> ->  Declarando uma lista chamado que não pode ter valor de repetido
-  
-  * new HashSet<> -> declarando um novo index / 'pasta organizada' de dependentes do 'FUNCIONARIO'
+   private Set<Dependente> dependentes = new HashSet<>();
+  /*
 
-  */
+   *  Set <tipo da lista> ->  Declarando uma lista chamado que não pode ter valor de repetido
 
-   public Funcionario(String nome, String cpf, Date dataNacimento, double salarioBruto, Set<Dependente> dependentes) {
+   * new HashSet<> -> declarando um novo index / 'pasta organizada' de dependentes do 'FUNCIONARIO'
+
+   */
+
+  public Funcionario(String nome, String cpf, Date dataNacimento, double salarioBruto) {
     super(nome, cpf, dataNacimento);
     this.salarioBruto = salarioBruto;
-    this.dependentes = dependentes;
   }
 
   /* GET */
-  public double getSalarioBruto(){
+  public double getSalarioBruto() {
     return this.salarioBruto;
   }
 
-  public int getId_funcionario(){
+  public int getId_funcionario() {
     return this.id_funcionario;
   }
 
-  public Set<Dependente> getDependentes() {
+   public Set<Dependente> getDependentes() {
     return dependentes;
   }
 
   /* SET */
-  public void setSalarioBruto(double salarioBruto){ 
+  public void setSalarioBruto(double salarioBruto) {
     this.salarioBruto = salarioBruto;
   }
-
-  public void setDependentes (Set<Dependente> dependentes) {
-    this.dependentes = dependentes;
-  }
-
 }
+ // public void setDependentes (Set<Dependente> dependentes) {
+  //  this.dependentes = dependentes;
+ // }
+
+//}
