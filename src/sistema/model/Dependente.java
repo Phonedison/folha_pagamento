473671683fa28e9b92272835a441c34267257c5b
vLamass <viniciusslamas17@gmail.com>
2026-04-20 21:53:38 -0300

    20/04/26 - Correção de Alguns Comentarios e Adição de Outros.

--- src/sistema/model/Dependente.java
+++ src/sistema/model/Dependente.java
@@ -5,34 +5,42 @@ import java.time.Period;
 import sistema.enums.Parentesco;
 import sistema.exception.DependenteException;
 
+// Classe Dependente herda de Pessoa, ou seja,
+// possui todos os atributos e comportamentos de Pessoa
 public class Dependente extends Pessoa {
-
+  // Criação de variaveis.
   private Integer funcionario;
   private Parentesco parentesco;
   private Integer id_dependente;
 
+  // Construtor Vazio.
   public Dependente() {
     super();
   }
 
+  // Construtor com atributos.
   public Dependente(String nome, String cpf, LocalDate dataNascimento, Funcionario funcionario, Parentesco parentesco) {
     super(nome, cpf, dataNascimento);
     this.funcionario = funcionario.getId_funcionario();
     this.parentesco = parentesco;
   }
 
+  // get Parentesco
   public Parentesco getParentesco() {
     return this.parentesco;
   }
 
+  // get id_dependente
   public Integer getId_dependente() {
     return this.id_dependente;
   }
 
+  // set Parentesco
   public void setParentesco(Parentesco parente) {
     this.parentesco = parente;
   }
 
+  // Método para escolher parentesco usando String.
   public void escolherParentesco(String opcao) {
 
     switch (opcao) {
@@ -46,6 +54,7 @@ public class Dependente extends Pessoa {
     }
   }
 
+  // Sobrecarga do método escolherParentesco usando número inteiro.
   public void escolherParentesco(int opcao) {
 
     switch (opcao) {
@@ -58,14 +67,17 @@ public class Dependente extends Pessoa {
     }
   }
 
+  // get do Funcionario
   public Integer getFuncionario() {
     return this.funcionario;
   }
 
+  // set do funcionario
   public void setFuncionario(Integer funcionario) {
     this.funcionario = funcionario;
   }
 
+  // Função para Calcular a idade deixando registrar apenas < 18.
   public void validarDependente() throws DependenteException {
     int idade = Period.between(getDataNacimento(), LocalDate.now()).getYears();
     if (idade > 18) {
