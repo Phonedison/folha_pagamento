ac017881c96c1540f214545e8a130c2deb91bee7
vLamass <viniciusslamas17@gmail.com>
2026-04-20 22:06:50 -0300

    20/04/26 - Acho que Agora Comentei Tudo

--- src/sistema/model/Pessoa.java
+++ src/sistema/model/Pessoa.java
@@ -3,14 +3,16 @@ package sistema.model;
 import java.time.LocalDate;
 
 public abstract class Pessoa {
-
+  // Desclaração de Variaveis.
   protected String nome;
   protected String cpf;
   protected LocalDate dataNascimento;
 
+  // Construtor Vazio.
   public Pessoa() {
   }
 
+  // Contrutor com Parametros.
   public Pessoa(String nome, String cpf, LocalDate dataNascimento) {
     this.nome = nome;
     this.cpf = cpf;
@@ -36,16 +38,19 @@ public abstract class Pessoa {
   }
 
   public void setCpf(String cpf) {
+    // Remove tudo que não for número do CPF (pontos, traços, espaços etc.).
     String cpfClean = cpf.replaceAll("\\D", "");
 
+    // Valida se o CPF possui exatamente 11 dígitos após a limpeza.
     if (cpfClean.length() != 11) {
       throw new IllegalArgumentException("CPF inválido");
     }
-
+    // Atribui o CPF já limpo (somente números) ao atributo da classe.
     this.cpf = cpfClean;
   }
 
   public void setDataNacimento(LocalDate dataNascimento) {
+    // Define a data de nascimento do objeto.
     this.dataNascimento = dataNascimento;
   }
 }
